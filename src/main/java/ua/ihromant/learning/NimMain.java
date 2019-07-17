8110fe362b4aa1706cd43b8305a4bff1b68f8aab
ihetman <ivan.hetman@lebara.com>
2019-07-17 17:11:37 +0300

    Changed main

--- src/main/java/ua/ihromant/learning/NimMain.java
+++ src/main/java/ua/ihromant/learning/NimMain.java
@@ -5,29 +5,29 @@ import java.util.Scanner;
 
 import ua.ihromant.learning.ai.AITemplate;
 import ua.ihromant.learning.ai.QLearningTemplate;
-import ua.ihromant.learning.state.NimState;
+import ua.ihromant.learning.state.NimLineState;
 import ua.ihromant.learning.state.Player;
 
 public class NimMain {
 	// private static AITemplate template = new MinimaxTemplate(Player.O);
-	private static AITemplate template = new QLearningTemplate(new NimState(new int[] {1, 3, 5, 7}), 100000);
+	private static AITemplate template = new QLearningTemplate(new NimLineState(new int[] {1, 3, 5, 7}), 100000);
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		NimState state = new NimState(new int[] {1, 3, 5, 7});
+		NimLineState state = new NimLineState(new int[] {1, 3, 5, 7});
 		while (!state.isTerminal()) {
-			state = (NimState) template.decision(state).getTo();
+			state = (NimLineState) template.decision(state).getTo();
 			System.out.println(state.toString());
 			if (state.isTerminal()) {
 				break;
 			}
-			System.out.println("Enter your move, firstly number to take, then indices");
+			System.out.println("Enter your move, firstly number to take, then index (starting from 0)");
 			int toTake = Integer.parseInt(scan.nextLine());
-			state = new NimState(state,
-					Arrays.stream(scan.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray(),
+			state = new NimLineState(state,
+					Integer.parseInt(scan.nextLine()),
 					toTake);
 			System.out.println(state.toString());
 		}
-		switch ((int) state.getUtility(Player.X)) {
+		switch ((int) state.getUtility(Player.O)) {
 			case 0:
 				System.out.println("Draw!");
 				return;
