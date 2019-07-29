3b2cc3e90eef7977f3f5fd1edfe1ec588beb83cb
ihetman <ivan.hetman@lebara.com>
2019-07-29 11:08:03 +0300

    Made FizzBuz working

--- src/main/java/ua/ihromant/learning/ai/qtable/TF1.java
+++ src/main/java/ua/ihromant/learning/ai/qtable/TF1.java
@@ -1,5 +1,6 @@
 package ua.ihromant.learning.ai.qtable;
 
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
@@ -9,6 +10,7 @@ import org.deeplearning4j.nn.weights.WeightInit;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.learning.config.AdaGrad;
 import org.nd4j.linalg.learning.config.Adam;
+import org.nd4j.linalg.learning.config.Nesterovs;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 public class TF1 {
@@ -17,14 +19,16 @@ public class TF1 {
 	private static MultiLayerConfiguration buildGraph() {
 		return new NeuralNetConfiguration.Builder()
 				.seed(1000)
-				.updater(new Adam())
+				.weightInit(WeightInit.XAVIER)
+				//.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+				.updater(new Nesterovs())
 				.list()
 				.layer(0, new DenseLayer.Builder().nIn(10).nOut(100)
-						.weightInit(WeightInit.ZERO)
-						.activation(Activation.SIGMOID)
+						//.weightInit(WeightInit.ZERO)
+						.activation(Activation.RELU)
 						.build())
 				.layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.RECONSTRUCTION_CROSSENTROPY)
-						.activation(Activation.IDENTITY)
+						.activation(Activation.SOFTMAX)
 						.nIn(100).nOut(4).build())
 				.build();
 	}
