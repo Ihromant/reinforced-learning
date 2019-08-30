05f4ec4398d4fe13fb8f6fe6edd1a1c936d27d65
ihetman <ivan.hetman@lebara.com>
2019-08-30 15:15:37 +0300

    Changed MiniMaxAI to choose random from possible moves

--- shared/src/main/java/ua/ihromant/learning/ai/MinimaxAI.java
+++ shared/src/main/java/ua/ihromant/learning/ai/MinimaxAI.java
@@ -4,8 +4,11 @@ import ua.ihromant.learning.agent.Agent;
 import ua.ihromant.learning.state.GameResult;
 import ua.ihromant.learning.state.Player;
 import ua.ihromant.learning.state.State;
+import ua.ihromant.learning.util.MaxUtil;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public final class MinimaxAI<A> implements Agent<A> {
     private final Player player;
@@ -15,7 +18,8 @@ public final class MinimaxAI<A> implements Agent<A> {
 
     @Override
     public State<A> decision(State<A> from) {
-        return from.getStates().max(Comparator.comparing(this::minValue)).orElseThrow(IllegalStateException::new);
+        List<State<A>> maxStates = from.getStates().collect(MaxUtil.maxList(Comparator.comparing(this::minValue)));
+        return maxStates.get(ThreadLocalRandom.current().nextInt(maxStates.size()));
     }
 
     private GameResult maxValue(State<A> state) {
