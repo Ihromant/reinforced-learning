1c2afd4b167f3189e6c3a4852962f030047b3b1d
ihetman <ivan.hetman@lebara.com>
2019-10-04 16:12:20 +0300

    Added debugger to spot broken neural feed

--- training/src/main/java/ua/ihromant/reinforced/ai/Trainer.java
+++ training/src/main/java/ua/ihromant/reinforced/ai/Trainer.java
@@ -1,21 +1,23 @@
 package ua.ihromant.reinforced.ai;
 
-import ch.qos.logback.classic.Level;
+//import ch.qos.logback.classic.Level;
 import org.slf4j.LoggerFactory;
+import ua.ihromant.learning.state.NimLineState;
+import ua.ihromant.learning.state.NimState;
 import ua.ihromant.learning.state.TicTacToeState3x3;
 import ua.ihromant.reinforced.ai.factory.TrainerFactory;
-import ch.qos.logback.classic.Logger;
+//import ch.qos.logback.classic.Logger;
 
 import java.io.File;
 
 public class Trainer {
     static {
-        Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-        root.setLevel(Level.INFO);
+        //Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        //root.setLevel(Level.INFO);
     }
 
     public static void main(String[] args) {
-        String path = System.getProperty("java.io.tmpdir") + File.separator + "3x3.ai";
-        TrainerFactory.newTicTacToeAgent(TicTacToeState3x3::new).train(100_000, path);
+        String path = System.getProperty("java.io.tmpdir") + File.separator + "nimMap.ai";
+        TrainerFactory.newNimAgent(() -> new NimLineState(new int[] {1, 3, 5, 7})).train(20_000, path);
     }
 }
